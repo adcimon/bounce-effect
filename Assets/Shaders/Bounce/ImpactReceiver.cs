398f8070d464398f3ff2f105654b32ef9a080a6a
adcimon <37084114+adcimon@users.noreply.github.com>
2019-03-19 16:58:05 +0100

    First commit

---
+++ Assets/Shaders/Bounce/ImpactReceiver.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class ImpactReceiver : MonoBehaviour
+{
+    public AnimationCurve curve;
+    public float damageRadius = 1;
+    public float bounceAmplitude = 1;
+    public float totalTime = 1;
+
+    private Material material;
+    private bool isAnimating = false;
+    private float currentTime = 0;
+
+    private void Awake()
+    {
+        material = GetComponent<MeshRenderer>().material;
+    }
+
+    private void Update()
+    {
+        if( isAnimating )
+        {
+            currentTime += Time.deltaTime;
+            material.SetFloat("_AnimationValue", curve.Evaluate(currentTime / totalTime) * bounceAmplitude);
+        }
+
+        if( currentTime > totalTime )
+        {
+            currentTime = 0;
+            isAnimating = false;
+            material.SetInt("_Bounce", 0);
+        }
+    }
+
+    public void Impact( Vector3 position, Vector3 direction )
+    {
+        if( curve == null || !material )
+        {
+        	return;
+        }
+
+        material.SetVector("_ImpactPosition", position);
+        material.SetVector("_ImpactDirection", direction);
+        material.SetFloat("_DamageRadius", damageRadius);
+        material.SetFloat("_AnimationValue", curve.Evaluate(currentTime / totalTime) * bounceAmplitude);
+        material.SetInt("_Bounce", 1);
+        currentTime = 0;
+        isAnimating = true;
+    }
+}
---
+++ Assets/Shaders/Bounce/ImpactSender.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class ImpactSender : MonoBehaviour
+{
+    private ImpactReceiver receiver;
+
+    private void Update()
+    {
+        if( Input.GetMouseButtonDown(0) )
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if( Physics.Raycast(ray, out hit) )
+            {
+                Debug.DrawLine(ray.origin, hit.point, Color.red);
+                receiver = hit.transform.GetComponent<ImpactReceiver>();
+            }
+        }
+
+        if( receiver && Input.GetMouseButtonUp(0) )
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if( Physics.Raycast(ray, out hit) )
+            {
+                if( hit.transform.GetComponent<ImpactReceiver>() == receiver )
+                {
+                    receiver.Impact(hit.point, ray.direction);
+                    receiver = null;
+                }
+            }
+        }
+    }
+}
